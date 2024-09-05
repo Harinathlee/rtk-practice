9cd4d9f5d8812def8b3075838c17363a99fa48c6
Harinath <Harinathlee@users.noreply.github.com>
2024-09-05 17:34:36 +0530

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// ES Module (converted postcss.config.js)
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Adjust the paths to match your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        // Custom min-width and max-width ranges
+        'mobile': { 'min': '320px', 'max': '639px' }, // Mobile range
+        'tablet': { 'min': '640px', 'max': '900px' }, // Tablet range
+        'desktop': { 'min': '901px' } // Desktop range
+        //'large-desktop': { 'min': '1024px', 'max': '1279px' }, // Large Desktop range
+        //'extra-large': { 'min': '1280px', 'max': '1535px' }, // Extra Large range
+      },
+    },
+  },
+  plugins: [],
+}
